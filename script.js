5ff8eec6b0ddcbdd458096f1e1e550cfdf620a99
Keketso <kekekeketso03@gmail.com>
2025-06-23 08:11:56 +0200

    AddcalculateHighscore

--- script.js
+++ script.js
@@ -10,22 +10,24 @@
 // console.log(document.querySelector('.guess').value);
 
 // document.querySelector('.check').addEventListener('click',);
-document.querySelector('.again').addEventListener('click', function(){
+document.querySelector('.again').addEventListener('click', function () {
   let score = 20;
+  
+  const secretNumber = Math.trunc(Math.random() * 20 + 1);
   document.querySelector('.score').textContent = 20;
-   document.querySelector('.message').textContent =
-      'Start guessing...';
-      document.querySelector('.guess').value = '';
-      document.querySelector('body').style.backgroundColor='#222';
-})
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+});
 const number = Math.random();
 let score = 20;
+let highscore = 0;
 const secretNumber = Math.trunc(Math.random() * 20 + 1);
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
-   if (guess < 0) {
+  if (guess < 0) {
     document.querySelector('.message').textContent =
       '❌ Negitive number not allowed!';
   } else if (!guess) {
@@ -50,8 +52,11 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '✔️💯🎉 Correct Number';
     document.querySelector('.score').textContent = score;
-    
+    document.querySelector('body').style.backgroundColor = '#60b347';
 
-    document.querySelector('body').style.backgroundColor='#60b347';
+    if (highscore < score) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   }
 });
